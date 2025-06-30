349e95d2d37b14c6ca5d958e24052da157dc3ec9
Stephanie Frantz <stephlage@gmail.com>
2025-06-30 09:42:32 -0400

    cleaner

--- quartz/quartz/i18n/locales/en-US.ts
+++ quartz/quartz/i18n/locales/en-US.ts
@@ -36,7 +36,7 @@ export default {
       title: "Explorer",
     },
     footer: {
-      createdWith: "Created with",
+      createdWith: "Created with curiosity by The Handsome Zebra using",
     },
     graph: {
       title: "Graph View",

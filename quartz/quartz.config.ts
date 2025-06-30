3a3a9791790f114c182e0d4c063708bd3a9028af
Stephanie Frantz <stephlage@gmail.com>
2025-06-30 10:49:34 -0400

    changed endpoint

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Thread Actor Garden",
+    pageTitle: "Threat Intelligence Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "threatactors.garden.handsomezebra.com",
+    baseUrl: "threatintelligence.garden.handsomezebra.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
